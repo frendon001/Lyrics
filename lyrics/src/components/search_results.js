0fe59bfb7af939a4f76976c971943b540324e8bc
arendon094 <arendon094@gmail.com>
2017-11-28 21:39:17 -0800

    Modified css for search results. Displaying all

--- lyrics/src/components/search_results.js
+++ lyrics/src/components/search_results.js
@@ -1,29 +1,43 @@
 import React from 'react';
 
-function result(track){
-  // console.log(track.primary_genres.music_genre_list[0].music_genre_name);
+function getTrack(trackParent){
+  let track = trackParent.track;
   return(
-    <div className="track-result-box">
+    <div className="track-result-box" key={track.track_id}>
       <p><strong>{track.track_name}</strong> BY <strong>{track.artist_name}</strong> FROM <strong>{track.album_name}</strong></p>
-      <p>Type of music: <em>{getGenres(track.primary_genres.music_genre_list)}</em></p>
+      <p>Type of music: {getGenres(track.primary_genres.music_genre_list)}</p>
       <p>The lyrics to this song from Musixmatch can be found <a href={track.track_share_url}>here</a></p>
     </div>
   );
 }
 
 function getGenres(genres){
-  return genres.map(genre => {
-    let g = ' | ' + genre.music_genre.music_genre_name + ' ';
-    return g;
-  })
+  return genres.map((genre, index) => {
+    let g = genre.music_genre;
+    if(index === 0){
+      return(
+        <em key={g.music_genre_id}>{g.music_genre_name}</em>
+      );
+    }
+    else{
+      return(
+        <em key={g.music_genre_id}> | {g.music_genre_name}</em>
+      );
+    }
+    
+  });
+}
+
+function renderTracks(tracks){
+  return tracks.map(track => {
+    return getTrack(track);
+  });
 }
 
 const renderResults = ({tracks}) => {
-  // tracks[0].track.track_name
-  console.log('Results Component: ', tracks);
   return(
     <div className="container">
-      {result(tracks[0].track)}
+      {renderTracks(tracks)}
     </div>
   );
 }
