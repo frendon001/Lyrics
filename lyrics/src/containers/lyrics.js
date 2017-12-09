528f806c490331ea5adfceba371cd8f78d529d3a
arendon094 <arendon094@gmail.com>
2017-12-08 17:24:13 -0800

    Set up index.js to handle routes. Moved Search nav bar

--- lyrics/src/containers/lyrics.js
+++ lyrics/src/containers/lyrics.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import SearchNav from './search_nav';
 import Welcome from '../components/welcome';
 import SearchResults from '../components/search_results';
 import { connect } from 'react-redux';
@@ -10,7 +9,6 @@ class Lyric extends Component {
     let display = !this.props.tracks ? <Welcome /> : <SearchResults tracks={this.props.tracks} />
     return(
       <div>
-        <SearchNav />
         {display}
       </div>
     );
--- lyrics/src/index.js
+++ lyrics/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
+import SearchNav from './containers/search_nav';
 import Lyrics from './containers/lyrics';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -14,7 +15,14 @@ const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-        <Lyrics />
+        <BrowserRouter>
+            <div>
+                <SearchNav />
+                <Switch>
+                    <Route exact path="/" component={Lyrics} />
+                </Switch>
+            </div>
+        </BrowserRouter>
     </Provider>
     , document.getElementById('root')
 );
