7e874440ac7f017e0a09773b4077818456e838d6
arendon094 <arendon094@gmail.com>
2017-12-13 20:58:02 -0800

    Fixed issue where special characters affected the url for song_details

--- lyrics/src/components/search_results.js
+++ lyrics/src/components/search_results.js
@@ -6,9 +6,14 @@ function getTrack(track){
     <div className="track-result-box" key={track.id}>
       <p><strong>{track.title}</strong> BY <strong>{track.primary_artist.name}</strong></p>
       <p>The lyrics to this song from Genius can be found <a href={track.url} target="_blank">here</a></p>
-      <div><Link className="btn btn-success" to={`/song/${track.title}&${track.primary_artist.name}&${track.id}`}>{track.title}</Link></div>
+      <div><Link className="btn btn-success" to={`/song/${getURLSafeString(track.title)}&${getURLSafeString(track.primary_artist.name)}&${track.id}`}>{track.title}</Link></div>
     </div>
   );
+}
+
+function getURLSafeString(unsafeString){
+  let safeString = unsafeString.replace(/[^a-zA-Z ]/g, "");
+  return safeString;
 }
 
 function renderTracks(hits){
