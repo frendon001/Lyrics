60df8045eb7517effee115028b8b28167f64dda2
arendon094 <arendon094@gmail.com>
2017-12-13 21:00:33 -0800

    Added spacing for track album in track_info

--- lyrics/src/components/track_info.js
+++ lyrics/src/components/track_info.js
@@ -5,7 +5,7 @@ const renderInfo = ({ track }) => {
     <div>
       <h2><em>{track.title}</em> by <em>{track.primary_artist.name}</em></h2>
       <img src={track.album.cover_art_url} alt="Album Cover" width="70%" />
-      <h5><em>{track.album.name}</em></h5>
+      <h5 className="track-album"><em>{track.album.name}</em></h5>
       <hr />
     </div>
   );
