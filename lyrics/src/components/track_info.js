790b8f6faa035b4d97ec45d749026269ae4e1f15
arendon094 <arendon094@gmail.com>
2017-12-15 20:03:35 -0800

    Added an image for when no album cover is available

--- lyrics/src/components/track_info.js
+++ lyrics/src/components/track_info.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import NoAlbum from '../images/no_album.png';
 
 const renderInfo = ({ track }) => {
 
@@ -6,7 +7,7 @@ const renderInfo = ({ track }) => {
   return(
     <div>
       <h2><em>{track.title}</em> by <em>{track.primary_artist.name}</em></h2>
-      <img src={track.album ? track.album.cover_art_url : "http://www.lovespirals.com/wp-content/themes/soundcheck212/images/default-artwork.png"} alt="Album Cover" width="70%" />
+      <img src={track.album ? track.album.cover_art_url : NoAlbum} alt="Album Cover" width="70%" />
       <h5 className="track-album"><em>{track.album ? track.album.name : "Unknown Album"}</em></h5>
       <hr />
     </div>
