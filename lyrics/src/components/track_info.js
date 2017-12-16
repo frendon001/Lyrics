91eabc62cf5af26181342a8a2979e021b5132879
arendon094 <arendon094@gmail.com>
2017-12-15 19:36:49 -0800

    Handled null API info for album object from API

--- lyrics/src/components/track_info.js
+++ lyrics/src/components/track_info.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const renderInfo = ({ track }) => {
+
+
   return(
     <div>
       <h2><em>{track.title}</em> by <em>{track.primary_artist.name}</em></h2>
-      <img src={track.album.cover_art_url} alt="Album Cover" width="70%" />
-      <h5 className="track-album"><em>{track.album.name}</em></h5>
+      <img src={track.album ? track.album.cover_art_url : "http://www.lovespirals.com/wp-content/themes/soundcheck212/images/default-artwork.png"} alt="Album Cover" width="70%" />
+      <h5 className="track-album"><em>{track.album ? track.album.name : "Unknown Album"}</em></h5>
       <hr />
     </div>
   );
