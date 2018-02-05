8533192e03964069a314c2cb209a17539d864c67
arendon094 <arendon094@gmail.com>
2018-02-04 23:06:47 -0800

    Updated server image as a get route for no_album.png

--- lyrics_auth/router.js
+++ lyrics_auth/router.js
@@ -15,6 +15,9 @@ module.exports = function(app){
   app.get('/static/media/dj_set_index.87d8ee92.png', (req, res) => {
     res.sendFile(path.join(__dirname, "../lyrics/build/static/media/dj_set_index.87d8ee92.png"));
   });
+  app.get('/static/media/no_album.6bba9800.png', (req, res) => {
+    res.sendFile(path.join(__dirname, "../lyrics/build/static/media/no_album.6bba9800.png"));
+  });
   app.get('/song', Genius.song);
   app.get('/song/*', (req, res) => {
     res.sendFile(path.join(__dirname, "../lyrics/build/index.html"));
