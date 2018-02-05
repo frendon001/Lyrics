bd0ad9567b0b1ca95e2c827c91417b5890d7499d
arendon094 <arendon094@gmail.com>
2018-02-04 19:48:58 -0800

    Removed favicon, edited router

--- lyrics_auth/router.js
+++ lyrics_auth/router.js
@@ -2,13 +2,13 @@ const Genius = require('./controllers/genius');
 const config = require('./config');
 
 module.exports = function(app){
-  app.get('/song', Genius.song);
-  app.get('/search', Genius.search);
-  app.get('/lyrics', Genius.lyrics)
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     // res.sendFile(__dirname + "/public/index.html");
     // var testHtmlPath = path.resolve(__dirname, '..', 'lyrics', 'build', 'index.html');
     // res.sendFile(testHtmlPath);
     res.sendFile(path.join(__dirname, "../lyrics/build/index.html"));
-  })
+  });
+  app.get('/song', Genius.song);
+  app.get('/search', Genius.search);
+  app.get('/lyrics', Genius.lyrics);
 }
