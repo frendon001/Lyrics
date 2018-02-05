5bc2fde2b0445c6afee5dd9ab3b0713d150823d2
arendon094 <arendon094@gmail.com>
2018-02-04 20:26:35 -0800

    router edit

--- lyrics_auth/router.js
+++ lyrics_auth/router.js
@@ -2,12 +2,12 @@ const Genius = require('./controllers/genius');
 const config = require('./config');
 
 module.exports = function(app){
-  app.get('/', (req, res) => {
-    // res.sendFile(__dirname + "/public/index.html");
-    // var testHtmlPath = path.resolve(__dirname, '..', 'lyrics', 'build', 'index.html');
-    // res.sendFile(testHtmlPath);
-    res.sendFile(path.join(__dirname, "../lyrics/build/index.html"));
-  });
+  // app.get('/', (req, res) => {
+  //   // res.sendFile(__dirname + "/public/index.html");
+  //   // var testHtmlPath = path.resolve(__dirname, '..', 'lyrics', 'build', 'index.html');
+  //   // res.sendFile(testHtmlPath);
+  //   res.sendFile(path.join(__dirname, "../lyrics/build/index.html"));
+  // });
   app.get('/song', Genius.song);
   app.get('/search', Genius.search);
   app.get('/lyrics', Genius.lyrics);
