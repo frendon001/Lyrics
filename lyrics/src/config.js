4fd2d48d106a3a2ac58fa22b6230b58baea5ba35
arendon094 <arendon094@gmail.com>
2018-02-05 19:22:25 -0800

    removed cached files for gitignore

--- lyrics/src/config.js
+++
@@ -1,3 +0,0 @@
-exports.KEYS = {
-  google: 'AIzaSyAiAZTlUCAZDrorA0smIyRoGR8pOi1o-II'
-}
--- lyrics_auth/config.js
+++
@@ -1,7 +0,0 @@
-exports.config = {
-  // clientID: 'le-rYusZ3X40kmgyNlgKZk8AsIfNU7HQFWHLnWFei_3UayXpdbfSYDG0qyuScyR7',
-  // redirectURI: 'http://localhost:3030/callback',
-  // scope: 'me',
-  // clientSecret: 'hO50tYKGodQsSg4YLiGbxFQaJgZTrZ8MUyGKEcNnoo2WPIIV6lwM6d4nYbEgQIxFnR6FE6pHxjPYvTz1HPk48w',
-  access_token: '7E7OXRV_zO2146l42ElA8auQxFJSBLeyB9e0K76lQhB7Q4oLj-oUSrQ-0e_wDS1q'
-}
