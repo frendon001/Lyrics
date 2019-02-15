722096cfb8e8b316af234c3dace222d38cda2483
arendon094 <arendon094@gmail.com>
2019-02-14 22:34:30 -0800

    edited server index file

--- lyrics_auth/index.js
+++ lyrics_auth/index.js
@@ -12,6 +12,17 @@ app.use(bodyParser.json({ type: '*/*' }));
 
 router(app);
 
+if (process.env.NODE_ENV === 'production') {
+    //Express to serve up production assets
+    app.use(express.static('client/build'));
+    //Expess will serve up index.html file
+    //if route is not recognized
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 const port = process.env.PORT || 3030;
 const server = http.createServer(app);
 server.listen(port);
